df4e99960d5441ca8be49956df7a7f8ba44b2774
Lazare Fortune <lazarefortune@gmail.com>
2023-09-22 22:32:11 +0200

    style: update global style and login/register form

--- assets/js/base/index.js
+++ assets/js/base/index.js
@@ -4,18 +4,25 @@ createIcons({icons});
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // user dropdown
-    const userDropdownIcon = document.querySelector('#user-dropdown-icon');
-    const userDropdown = document.querySelector('#user-dropdown');
+    // // user dropdown
+    // const userDropdownIcon = document.querySelector('#user-dropdown-icon');
+    // const userDropdown = document.querySelector('#user-dropdown');
+    //
+    // userDropdownIcon.addEventListener('click', () => {
+    //     userDropdown.classList.toggle('hidden');
+    // });
+    //
+    // // click outside to close dropdown
+    // document.addEventListener('click', (e) => {
+    //     if (!userDropdownIcon.contains(e.target)) {
+    //         userDropdown.classList.add('hidden');
+    //     }
+    // });
 
-    userDropdownIcon.addEventListener('click', () => {
-        userDropdown.classList.toggle('hidden');
-    });
 
-    // click outside to close dropdown
-    document.addEventListener('click', (e) => {
-        if (!userDropdownIcon.contains(e.target)) {
-            userDropdown.classList.add('hidden');
-        }
+    // Show dropdown menu on user button click
+    document.getElementById('userBtn').addEventListener('click', function () {
+        // show dropdown menu with animation
+        document.getElementById('userDropdown').classList.toggle('scale-100');
     });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          // pink colors from tailwindcss
+          "50": "rgb(253 242 248)",
+          "100": "rgb(252 231 243)",
+          "200": "rgb(251 207 232)",
+          "300": "rgb(249 168 212)",
+          "400": "rgb(244 114 182)",
+          "500": "rgb(236 72 153)",
+          "600": "rgb(219 39 119)",
+          "700": "rgb(190 24 93)",
+          "800": "rgb(157 23 77)",
+          "900": "rgb(131 24 67)",
+          "950": "rgb(190 24 93)"
+        },
+        danger: {
+          "50": "#fff8f8",
+          "100": "#ffefef",
+          "200": "#ffd7d7",
+          "300": "#ffafaf",
+          "400": "#ff7d7d",
+          "500": "#ff4a4a",
+          "600": "#ff1f1f",
+          "700": "#ff0b0b",
+          "800": "#e60000",
+          "900": "#c50000",
+          "950": "#8c0000"
+        }
+      }
+    },
     fontFamily: {
       'inter': ['Inter'],
       'josefin': ['JosefinSans'],
@@ -16,8 +46,11 @@ module.exports = {
       'plusJakartaSans': ['Plus Jakarta Sans'],
       'ggSans': ['GG Sans'],
       'GlacialIndifference': ['Glacial Indifference'],
+      'IBM': ['IBM Plex Sans'],
     }
   },
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/forms'),
+  ],
 }
 
